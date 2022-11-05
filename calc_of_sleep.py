01d400eaaa7f84494b79316af74daa35079ce22a
Dmytro <116837456+DemaRedgrave@users.noreply.github.com>
2022-11-05 17:07:49 -0400

    Add files via upload

---
+++ calc_of_sleep.py
@@ -0,0 +1,8 @@
+print("How long do you sleep at night? (In hours)")
+X = int(input())
+print("Now enter how long do you sleep at the daytime (In minutes)")
+Y = int(input())
+print('Please wait... I multiply in my mind')
+print(X * 60 + Y)
+print("There is how much do you sleep in the one day")
+
